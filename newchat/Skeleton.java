2452e01e9d2aa0616d3e8ba05003352a34546bb2
Q456lm <cbbunting1@my.waketech.edu>
2025-11-19 09:09:42 -0500

    Create Skeleton.java

---
+++ newchat/Skeleton.java
@@ -0,0 +1,12 @@
+// Skeleton enemy subclass
+ 
+// TODO: Extend Enemy
+ 
+public class Skeleton {
+ 
+    // TODO: Create constructor (call super)
+ 
+    // TODO: Implement update()
+ 
+    // TODO: Implement attack()
+}
