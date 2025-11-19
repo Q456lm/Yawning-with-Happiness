9b7354ba219e082bb3a74878dd524f25cd1dfa5b
Q456lm <cbbunting1@my.waketech.edu>
2025-11-19 09:09:59 -0500

    Create Dragon.java

---
+++ newchat/Dragon.java
@@ -0,0 +1,12 @@
+// Dragon enemy subclass
+ 
+// TODO: Extend Enemy
+ 
+public class Dragon {
+ 
+    // TODO: Create constructor (call super)
+ 
+    // TODO: Implement update()
+ 
+    // TODO: Implement attack()
+}
