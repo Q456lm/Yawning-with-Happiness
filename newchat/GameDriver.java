06084f85c3e45cb74980fe658b98980b1fbab23f
Q456lm <cbbunting1@my.waketech.edu>
2025-11-19 09:10:14 -0500

    Create GameDriver.java

---
+++ newchat/GameDriver.java
@@ -0,0 +1,20 @@
+// Driver for the Enemy inheritance lab
+ 
+public class GameDriver {
+ 
+    public static void main(String[] args) {
+ 
+        // TODO: Add this commented line to show that Enemy cannot be instantiated:
+        // Enemy e = new Enemy();
+ 
+        // TODO: Create an Enemy[] array with:
+        // new Slime(...)
+        // new Skeleton(...)
+        // new Dragon(...)
+ 
+        // TODO: Loop through the array and call:
+        // update(), attack(), takeDamage()
+ 
+        // TODO: Add reflection answers as comments at the bottom of this file
+    }
+}
