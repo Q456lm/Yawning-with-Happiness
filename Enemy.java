af1585c5574e3fac905bceca820ccb447b5785bc
Q456lm <cbbunting1@my.waketech.edu>
2025-11-19 09:07:52 -0500

    Create Enemy.java

---
+++ Enemy.java
@@ -0,0 +1,18 @@
+// Lab: Abstract Enemies
+// TODO: Declare this class as abstract
+ 
+public class Enemy {
+ 
+    // TODO: Add protected instance variables:
+    // health, damage, name
+ 
+    // TODO: Create a constructor that initializes all fields
+ 
+    // TODO: Declare abstract methods:
+    // update()
+    // attack()
+ 
+    // TODO: Create a concrete takeDamage(int amount) method
+ 
+    // TODO: Add a getName() method
+}
